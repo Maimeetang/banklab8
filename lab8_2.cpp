23abfc7ed592660c4490e56edc49042c95983d9f
Bank <sirawit.36298@gmail.com>
2022-12-20 16:08:12 +0700

    lab8_2.cpp completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,41 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    //ถามชื่อ
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;
+
+    //ถามรหัส
+    int N;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl << name << ": ";
+    cin >> N;
+    cin.ignore();
+    N = (N/10000000)-12;
+    cout << "Fahsai: I think you may be GEAR " << N << ". I have a free movie ticket for you." << endl;
+
+    //ชวนดูหนัง
+    string Movie,Day,smt;
+    char x = 'A';
+    x = 'z'+27;
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl;
+    cout << "Fahsai: What movie do you want to watch?" << endl;
+    cout << name << ": ";
+    getline(cin,Movie);
+    cout << "Fahsai: So....which day are you free to go with me?" << endl << name << ": ";
+    getline(cin,Day);
+    cout << "Fahsai: " << Day << "....that is OK!!! I'm looking forward to watching " << Movie << " with you." << endl << name << ": ";
+    cin >> smt;
+    cin.ignore();
+    cout << "Fahsai: 555+ see you " << Day << ". Bye Bye " << x << "(^ ^)/";
+
+    return 0;
+}
