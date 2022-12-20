1fb95600d42ce5bd3e4989a5407bededd8e180f5
Bank <sirawit.36298@gmail.com>
2022-12-20 16:45:46 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if(x>='A' and x<='Z'){
+        if(x=='A'){
+            return 'Z';
+        }
+        if(x>'A' and x<='Z'){
+            return x-1;
+        }else{
+        return '0';
+        }
+    }else{
+    return '0';
+    }
 }
 
 int main(){
